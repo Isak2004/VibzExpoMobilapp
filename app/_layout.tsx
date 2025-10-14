b4376d1421aca7522083149f6632c3a795c140c2
Isak2004 <grabbstah@gmail.com>
2025-10-14 23:00:21 +0200

    Updated package-lock.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (Platform.OS === 'android') {
       NavigationBar.setVisibilityAsync('hidden');
-      NavigationBar.setBehaviorAsync('overlay-swipe');
+      NavigationBar.setBehaviorAsync('inset-swipe');
     }
   }, []);
 
