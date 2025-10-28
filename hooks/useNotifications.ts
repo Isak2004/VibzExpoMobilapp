50a7b04116a54818b63ee59f1ffae2e7264f1fe3
Isak2004 <grabbstah@gmail.com>
2025-10-28 18:29:52 +0100

    Updated useNotifications.ts

--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -57,10 +57,10 @@ export function useNotifications({
 
     return () => {
       if (notificationListener.current) {
-        Notifications.removeNotificationSubscription(notificationListener.current);
+        notificationListener.current.remove();
       }
       if (responseListener.current) {
-        Notifications.removeNotificationSubscription(responseListener.current);
+        responseListener.current.remove();
       }
     };
   }, [onTokenReceived, onNotificationReceived, onNotificationTapped]);
